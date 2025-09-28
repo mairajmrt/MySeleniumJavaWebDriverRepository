0ed30225c679fd1bd9790ed548251b244b437067
mali204 <mali204@DUC-LAn20q3gaZ3.dedalus.lan>
2025-09-28 17:49:17 +0530

    mmmmmmmmmm

---
+++ MySeleniumWebDriverProject/src/test/java/com/selenium/test/HomePageTest.java
@@ -0,0 +1,14 @@
+package com.selenium.test;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+
+	@Test
+
+	public void getHomePage() {
+		System.out.println("Welcome Home Page...");
+
+	}
+
+}
---
+++ MySeleniumWebDriverProject/src/test/java/com/selenium/test/LoginPageTest.java
@@ -0,0 +1,13 @@
+package com.selenium.test;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+	
+	@Test
+	
+	public void getLoginPage() {
+		System.out.println("Hello Login Page...");
+	}
+
+}
